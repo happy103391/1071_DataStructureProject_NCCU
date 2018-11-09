64cbffa78ecb252e92f20bd5ac481943c4b5ec68
tinyting <106306084@nccu.edu.tw>
2018-11-09 20:35:05 +0800

    新增了一個class，負責截取HTML的原始碼

---
+++ GQQGLE/src/htmlKeyword.java
@@ -0,0 +1,5 @@
+import java.*;
+public class htmlKeyword {   //擷取html的原始碼
+
+	
+}
--- GQQGLE/src/main.java
+++ GQQGLE/src/main.java
@@ -3,7 +3,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         System.out.println("這到底是三小");
+         System.out.println("Please enter the url:");
 	}
 
 }
