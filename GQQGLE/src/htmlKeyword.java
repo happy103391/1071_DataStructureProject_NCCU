3399eb254270b93de71bd3a173d8d927e6b4cd0e
tinyting <106306084@nccu.edu.tw>
2018-11-10 17:21:56 +0800

    之後再寫一個可以擷取網頁HTML原始檔的method

--- GQQGLE/src/htmlKeyword.java
+++ GQQGLE/src/htmlKeyword.java
@@ -1,5 +1,10 @@
 import java.*;
 public class htmlKeyword {   //擷取html的原始碼
-
+    private String urlStr;  //這是網址
+    private String content; //html原始碼的內容
+    
+    public htmlKeyword(String urlStr) {
+    	this.urlStr = urlStr;
+    }
 	
 }
