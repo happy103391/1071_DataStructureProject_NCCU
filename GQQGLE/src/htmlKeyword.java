54d11e74ca9d6c1376bf700b0d35a4f579ff8462
tinyting <106306084@nccu.edu.tw>
2018-11-10 23:27:50 +0800

    擷取URL

--- GQQGLE/src/htmlKeyword.java
+++ GQQGLE/src/htmlKeyword.java
@@ -1,11 +1,25 @@
 import java.*;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.*;
 public class htmlKeyword {   //擷取html的原始碼
     private String urlStr;  //這是網址
     private String content; //html原始碼的內容
     
-    public htmlKeyword(String urlStr) {
+    URL url;
+    HttpURLConnection urlCon;
+    BufferedReader urlIn;
+    String str;
+   
+    
+    public htmlKeyword(String urlStr) { 
     	this.urlStr = urlStr;
     }
     
-	
 }
+    
+    
+	
+
