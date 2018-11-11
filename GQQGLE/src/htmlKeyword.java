7b1d6f9010439a2b66cc24a10b34e2ddba10c5b3
tinyting <106306084@nccu.edu.tw>
2018-11-11 11:57:46 +0800

    之後用indexOf找關鍵字

--- GQQGLE/src/htmlKeyword.java
+++ GQQGLE/src/htmlKeyword.java
@@ -17,7 +17,27 @@ public class htmlKeyword {   //擷取html的原始碼
     public htmlKeyword(String urlStr) { 
     	this.urlStr = urlStr;
     }
+    private String fetchContent()throws IOException {    //網頁上的內容
+		URL url = new URL(this.urlStr);               //把使用者輸進去的網址傳入新的object裡，也就是url
+		URLConnection uc = url.openConnection();      //檢查網址的狀態是否存在or無法存取
+		InputStream is = uc.getInputStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(is)); //需要BufferedReader去讀網頁內容
+		
+		String retVal = " ";
+		String line = null;
+		while((line = br.readLine())!=null) {
+			retVal = retVal + line + "\n";
+		}
+		
+		return retVal;
+	}
+    public void match() throws IOException {     //也要拋出一個
+		if(content==null) {            //如果沒有HTML內容
+			content = fetchContent();      //去抓取html的原始碼
+			
+		}
     
+}
 }
     
     
