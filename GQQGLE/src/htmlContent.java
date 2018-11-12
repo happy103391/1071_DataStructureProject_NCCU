495ace388e045fa4a44fa552dbcf6868cb18a0dc
tinyting <106306084@nccu.edu.tw>
2018-11-12 16:22:46 +0800

    htmlKeyword改名字

--- GQQGLE/src/htmlContent.java
+++ GQQGLE/src/htmlContent.java
@@ -4,7 +4,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.*;
-public class htmlKeyword {   //擷取html的原始碼
+public class htmlContent {   //擷取html的原始碼
     private String urlStr;  //這是網址
     private String content; //html原始碼的內容
     
@@ -14,7 +14,7 @@ public class htmlKeyword {   //擷取html的原始碼
     String str;
    
     
-    public htmlKeyword(String urlStr) { 
+    public htmlContent(String urlStr) {
     	this.urlStr = urlStr;
     }
     private String fetchContent()throws IOException {    //網頁上的內容
