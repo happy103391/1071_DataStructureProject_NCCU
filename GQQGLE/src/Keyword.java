14015f98674543ef11f4aad9e04771bde2c2e443
tinyting <106306084@nccu.edu.tw>
2018-11-21 15:21:04 +0800

    create the class "Keyword"

---
+++ GQQGLE/src/Keyword.java
@@ -0,0 +1,13 @@
+
+public class Keyword {
+	
+		public String name;
+		public int count;
+		public double weight;
+		
+		public Keyword(String name,int count, double weight) {  //constructor
+			this.name = name;
+			this.weight = weight;
+			this.count= count;
+		}
+	}
