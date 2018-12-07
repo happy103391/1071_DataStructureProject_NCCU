c404b2e85c71269c720acf6d5e93dea1f7c5c1a9
tinyting <106306084@nccu.edu.tw>
2018-12-07 23:22:23 +0800

    new scanner

--- GQQGLE/src/main.java
+++ GQQGLE/src/main.java
@@ -1,10 +1,10 @@
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         System.out.println("Please enter the url:");
-         System.out.println("Hello!!!!");
+        Scanner sc = new Scanner(System.in);
 	}
 
 }
