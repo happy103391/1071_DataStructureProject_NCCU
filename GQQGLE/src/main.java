1b286da8d740211ada1e5109ca6e47ac0bd84bb3
tinyting <106306084@nccu.edu.tw>
2018-12-09 23:43:01 +0800

    輸入關鍵字

--- GQQGLE/src/main.java
+++ GQQGLE/src/main.java
@@ -4,6 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.print("請輸入關鍵字：");
         Scanner sc = new Scanner(System.in);
 	}
 
