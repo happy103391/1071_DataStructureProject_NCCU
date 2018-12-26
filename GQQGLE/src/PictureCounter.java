849cad9efe784e63c1fbcc5a9c845fc575540c22
liujasmine <liujasmine@10.232.197.168>
2018-12-26 16:41:30 +0800

    pictureCounter改囉

--- GQQGLE/src/PictureCounter.java
+++ GQQGLE/src/PictureCounter.java
@@ -1,8 +1,13 @@
 
 public class PictureCounter {			//(1)圖片"img"越多越好(2)"gif"比重佔多少
 	public String img;
-	public int piccount;					
-	public double picweight;				
+	public String gif;
+	public int imgcount;	
+	public int gifcount;
+	public int score;				//=((imgcount*imgweigh)t+(gifcount+gifweight))
+	public int imgweight = 1;
+	public int gifweight = 2;
+	//public double picweight;
 
 	public PictureCounter(String img, int piccount, int picweight) {
 		this.img = img;
