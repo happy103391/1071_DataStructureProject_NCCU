00106456a3be29213e6428f793537637492aa5ab
tinyting <106306084@nccu.edu.tw>
2018-12-28 13:10:08 +0800

    call google searching engine

--- GQQGLE/src/main.java
+++ GQQGLE/src/main.java
@@ -1,20 +1,17 @@
+import java.io.IOException;
 import java.util.Scanner;
-import java.*;
-import java.applet.*;
+
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		System.out.print("請輸入關鍵字：");
-        Scanner sc = new Scanner(System.in);
-        String a = sc.toString();
-        switch(a) {
-        	case "梗圖":
-        		Keyword aa = new Keyword(5.0);
-        		break;
-        	case "黃色":
-        		Keyword bb = new Keyword(3);
-        		break;
-        }
+		Scanner sc= new Scanner(System.in);
+		while(sc.hasNextLine()) {
+			String keyword =sc.next();
+			GoogleQuery googleQuery = new GoogleQuery(keyword);
+			googleQuery.query();
+		}
+		sc.close();
 	}
+
 }
