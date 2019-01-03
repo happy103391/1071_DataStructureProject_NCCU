7246038ec0b366a30fed319e2f3877b045837d16
liujasmine <liujasmine@10.232.197.168>
2019-01-03 23:13:42 +0800

    打到一半的findSubLink 還加了webPage裡一點內容

--- GQQGLE/src/Counter.java
+++ GQQGLE/src/Counter.java
@@ -7,36 +7,50 @@ public class Counter extends htmlContent  {
 		super(searchKeyword);
 		// TODO Auto-generated constructor stub
 	}
+	
 	public int countKeyword(String keyword) throws IOException {   //找關鍵字
-		 if(content == null) {
-	    	 content = fetchContent();    //去擷取內容
-	     }
-	    	 content = content.toUpperCase();   //把格式變一樣(都大寫)
-	    	 keyword = keyword.toUpperCase();
-	    	 //找出這個content有幾個keyword
-	    	 //to do : indexOf(keyword)
-	    	 int wcount =0;
-	    	 int i= content.indexOf(keyword);
-	    	 while(i!=-1) {
-	    		 wcount++;
+		if(content == null) {
+	    	content = fetchContent();    //去擷取內容
+	    }
+	    	
+		content = content.toUpperCase();   //把格式變一樣(都大寫)
+	    	keyword = keyword.toUpperCase();
+	    
+	    	//找出這個content有幾個keyword
+	    	//to do : indexOf(keyword)
+	    	int wcount = 0;
+	    	int i = content.indexOf(keyword);
+	    	while(i!=-1) {
+	    		wcount++;
 	    	    content = content.substring(i + keyword.length(), content.length()); //(從 i+keyword 開始,在content之前結束)
 	    	    i = content.indexOf(keyword);
-	    	 }return wcount;
-	      }
-	 public int countPic(String url) {
-		 int pcount=0;
-		 
-		 String img ="<img class=";
-		 int j = content.indexOf(img);
-		 while(j!=-1) {
-			 pcount++;
-			 content=content.substring(j+ img.length(), content.length());
-		 }
-		 return pcount;
-	 }
-	 public double calculate(int wcount,int pcount) {
-		 double score = (wcount*10)+(pcount*8);
-		 return score;
-	 }
+	    	}
+	    	return wcount;
 	}
+	
+	public int countPic(String url) {
+		int pcount = 0;
+		String img = "<img class=";
+		int j = content.indexOf(img);
+		while(j!=-1) {
+			 pcount++;
+			 content = content.substring(j+ img.length(), content.length());
+			 j = content.indexOf(img);
+		}
+		return pcount;
+	}
+	
+	public double calculate(int wcount,int pcount) {
+		double score = (wcount*10)+(pcount*8);
+		return score;
+	}
+	
+	public String findSubLinks() throws IOException {
+		if(content == null) {
+	    	content = fetchContent();    //去擷取內容
+	    }
+	    	
+		String subLink;
+	}
+}
 
--- GQQGLE/src/WebPage.java
+++ GQQGLE/src/WebPage.java
@@ -1,4 +1,28 @@
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class WebPage {
-
+	public String url;
+	public String name;
+	public Counter counter;
+	public GoogleQuery googleQuery;
+	public double score;
+	
+	public WebPage(String url, String name) {		//網址和關鍵字
+		this.url = url;
+		this.name = name;
+		this.counter = new Counter(url);
+	}
+	
+	public void setScore(ArrayList<Keyword> keywords) throws IOException {
+		this.score = 0;
+		
+		for(Keyword k: keywords) {										//從keywords陣列中的第一個數取到最後一個數
+			this.googleQuery = new GoogleQuery(k.name);
+			this.score += counter.countKeyword(k.name) * k.weight;		
+		}
+	}
+	
+	
 }
+
